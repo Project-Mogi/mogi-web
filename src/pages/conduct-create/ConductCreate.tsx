6ba8d0e45eae664c6bdcea574a2972e6965c523f
hyxx-su <legendary9899@dgsw.hs.kr>
2026-05-18 16:09:48 +0900

    chore: 임시 파일 삭제

--- src/pages/conduct-create/ConductCreate.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './ConductCreate.style';
-
-export function ConductCreatePage() {
-  return (
-    <S.Page>
-      <S.Title>상벌점 등록 페이지</S.Title>
-    </S.Page>
-  );
-}
--- src/pages/conduct-create/index.tsx
+++
@@ -1,1 +0,0 @@
-export { ConductCreatePage } from './ConductCreate';
