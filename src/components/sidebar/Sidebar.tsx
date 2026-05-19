b3f29118a20ec720160f8d9a64e00fe68dc6ea03
hyxx-su <legendary9899@dgsw.hs.kr>
2026-05-19 11:28:52 +0900

    fix: end 속성을 false로 수정

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import * as S from './Sidebar.style';
 const menuItems = [
   // { label: '대시보드', path: '/dashboard', end: true },
   // { label: '학생 관리', path: '/students' },
-  { label: '상벌점 관리', path: '/conduct', end: true },
+  { label: '상벌점 관리', path: '/conduct', end: false },
   // { label: '히스토리', path: '/histories' },
   // { label: '통계', path: '/statistics' },
 ];
